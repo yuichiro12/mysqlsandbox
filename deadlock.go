48b128208dc970c88d18b05808ec4cea37bae153
yuichiro12 <talcnrhyme@gmail.com>
2023-01-23 20:46:28 +0900

    reproduce simple deadlock

--- deadlock.go
+++ deadlock.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"crypto/md5"
 	"database/sql"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"sync"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func main() {
-	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/test")
+	src, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/test")
 	if err != nil {
 		log.Fatal("unable to use data source name", err)
 	}
+	db = src
 	defer db.Close()
 
-	if err = DropTableIfExists(db); err != nil {
+	if err = DropTableIfExists(); err != nil {
 		log.Fatal(err)
 	}
-	if err = CreateTable(db); err != nil {
-		log.Fatal(err)
-	}
-	if err = InsertSeed(db); err != nil {
+	if err = CreateTable(); err != nil {
 		log.Fatal(err)
 	}
 
@@ -36,9 +40,35 @@ func main() {
 	for rows.Next() {
 		fmt.Println(rows)
 	}
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go BatchInsert(wg)
+	}
+	wg.Wait()
+
 }
 
-func DropTableIfExists(db *sql.DB) error {
+func BatchInsert(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	s := ""
+	for i := 0; i < 1000; i++ {
+		rand.Seed(time.Now().UnixNano())
+		hex := md5.Sum([]byte(fmt.Sprintf("%d", rand.Intn(1000))))
+		s += fmt.Sprintf("('%s')", fmt.Sprintf("%x", hex))
+		if i != 999 {
+			s += ","
+		}
+	}
+	q := "INSERT IGNORE INTO t1 (name) VALUES " + s
+	if _, err := db.Exec(q); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func DropTableIfExists() error {
 	bytes, err := os.ReadFile("sql/t1_drop.sql")
 	if err != nil {
 		return err
@@ -47,7 +77,7 @@ func DropTableIfExists(db *sql.DB) error {
 	return err
 }
 
-func CreateTable(db *sql.DB) error {
+func CreateTable() error {
 	bytes, err := os.ReadFile("sql/t1.sql")
 	if err != nil {
 		return err
@@ -56,7 +86,7 @@ func CreateTable(db *sql.DB) error {
 	return err
 }
 
-func InsertSeed(db *sql.DB) error {
+func InsertSeed() error {
 	bytes, err := os.ReadFile("sql/seed.sql")
 	if err != nil {
 		return err
--- sql/t1.sql
+++ sql/t1.sql
@@ -1,4 +1,6 @@
-CREATE TABLE t1 (
-  id bigint,
-  name varchar(32)
-);
+CREATE TABLE `t1` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_unique` (`name`)
+) ENGINE=InnoDB;
